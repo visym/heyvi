1b10e014c934375a0d97d1d3df85b1fdc67380f1
Jeffrey Byrne <jeff@visym.com>
2021-08-27 04:16:24 +0000

    livestream to video

--- script/livestream.py
+++ script/livestream.py
@@ -1,9 +1,9 @@
 import os
 import vipy
 from argparse import ArgumentParser
+import heyvi.util
 
-
-def videostream():
+def youtube_livestream():
     assert 'VIPY_RTSP_KEY' in os.environ and 'VIPY_YOUTUBE_STREAM_KEY' in os.environ
     
     vi = vipy.video.Scene(url='rtsp://%s@10.0.1.14/live0' % os.environ['VIPY_RTSP_KEY'])
@@ -15,10 +15,27 @@ def videostream():
             s.write(im)
 
             
+def video_livestream(outfile):
+    assert 'VIPY_RTSP_KEY' in os.environ 
+    
+    vi = vipy.video.Scene(url='rtsp://%s@10.0.1.14/live0' % os.environ['VIPY_RTSP_KEY'])
+    vo = vipy.video.Scene(filename=outfile)
+    
+    with vo.stream(overwrite=True) as s:
+        for (k,im) in enumerate(vi.stream()):
+            s.write(im.annotate(timestamp=heyvi.util.timestamp()).rgb())
+            print(k,im)            
+            
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("-v","--video", help="Livestream video only", action='store_true')
+    parser.add_argument("-y","--youtube", help="Livestream video only to yourtube", action='store_true')
+    parser.add_argument("-v","--video", help="Livestream to an output video file")
     args = parser.parse_args()
 
-    if args.video:
-        videostream()
+    if args.video is not None:
+        video_livestream(args.video)
+    
+    elif args.youtube:
+        youtube_liveostream()
+
+        
