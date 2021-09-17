6bc1adb205b05ad378180a8c214914cf7b5d1b6b
Jeffrey Byrne <jeff@visym.com>
2021-09-17 10:31:21 -0400

    docs

--- heyvi/__init__.py
+++ heyvi/__init__.py
@@ -2,11 +2,25 @@
 
 # \"Hey Vi!\"
 
+HEYVI is a python package for visual AI that provides systems and trained models for activity detection and object tracking in videos.
+
+HEYVI provides:  
+
+* Real time activity detection of the [MEVA activity classes](https://mevadata.org)
+* Real time visual object tracking in long duration videos
+* Live streaming of annotated videos to youtube live
+* Visual AI from RTSP cameras
+
+
 ## Environment variables
+
+The following environment varibles may be set by the client:
 
 VIPY_RTSP_URL='rtsp://user@password:127.0.0.1'    
 VIPY_YOUTUBE_STREAMKEY='xxxx-xxxx-xxxx-xxxx-xxxx'    
 VIPY_CACHE='/home/username/.vipy'    
+
+For additional environment variables, refer to the vipy package.
 
 
 ## Versioning
